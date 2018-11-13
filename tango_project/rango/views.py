17eb5bc18ae832892e2cbdf3176228825371b7df
Pablo Molina <pablo.sbmol@gmail.com>
2018-11-12 23:24:11 -0300

    adding static and templates

--- tango_project/rango/views.py
+++ tango_project/rango/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("INDEX")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def rango(request):
